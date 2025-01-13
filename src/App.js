17846589412a078109960b1406b6cd0b00bfbb49
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-13 11:43:38 +0530

    JSX completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Dummy</h1>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2> Things to - DO </h2>
+      </div>
+      <div className="input">
+        <input type="text" placeholder="🖊️ Add item..." />
+        <i className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>React tutorial</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
