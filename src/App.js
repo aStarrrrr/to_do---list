8ff706dbb6b1a5153964f34fa2af3b1b1c31da37
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-14 10:37:00 +0530

    Basic functionality done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState("");
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -13,19 +17,55 @@ function App() {
       </div>
       <br />
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() => {
+            if (toDo.trim() !== "") {
+              setToDos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+              setToDo("");
+            }
+          }}
+          className="fas fa-plus"
+        ></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>React tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {toDos.map((obj) => {
+          return (
+            <div className="todo" key={obj.id}>
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    setToDos(
+                      toDos.map((item) =>
+                        item.id === obj.id
+                          ? { ...item, status: e.target.checked }
+                          : item
+                      )
+                    );
+                  }}
+                  checked={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i
+                  onClick={() =>
+                    setToDos(toDos.filter((item) => item.id !== obj.id))
+                  }
+                  className="fas fa-times"
+                ></i>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
